839b9d989cde208c49f008124165a25996c2e5c2
jrowle6776 <jrowle6776@CHHB2ZMKXV1.inst.hcpss.org>
2018-04-11 13:50:01 -0400

    Signed-off-by: jrowle6776 <jrowle6776@CHHB2ZMKXV1.inst.hcpss.org>

--- MissileCommand/src/MissileCommand.java
+++ MissileCommand/src/MissileCommand.java
@@ -1,6 +1,11 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
 import javax.swing.*;
 
 public class MissileCommand extends JPanel{
+	int x = 0;
+	int y = 0;
 	JFrame frame = new JFrame("Gay Command");
 	MissileCommand(){
 		frame.setContentPane(this);
@@ -8,6 +13,17 @@ public class MissileCommand extends JPanel{
 		frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
+		frame.repaint();
+	}
+	
+	public void paintComponent(Graphics g){
+		g.clearRect(x, y, 50, 50);
+		g.setColor(Color.pink);
+		g.drawOval(x, y, 50, 50);
+		
+		x++;
+		y++;
+		this.repaint();
 	}
 	
 	public static void main(String[] args){
