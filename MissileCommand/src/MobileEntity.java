d04d91a8bceaa2aa166a643f37b8169c8066d258
rupdik5087 <rupdik5087@CHHB2ZQKXV1.inst.hcpss.org>
2018-04-11 13:34:18 -0400

    poop

---
+++ MissileCommand/src/MobileEntity.java
@@ -0,0 +1,22 @@
+
+public abstract class MobileEntity {
+
+	int x, y;
+	double xVel, yVel;
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
+	public double getXVel() {
+		return xVel;
+	}
+	
+	public double getYVel() {
+		return yVel;
+	}
+}
