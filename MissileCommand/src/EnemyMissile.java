f5314d790085983e0ef17c28ffc442b32c7562f7
rupdik5087 <rupdik5087@CHHB2ZQKXV1.inst.hcpss.org>
2018-04-23 13:55:22 -0400

    A

--- MissileCommand/src/EnemyMissile.java
+++ MissileCommand/src/EnemyMissile.java
@@ -1,10 +1,10 @@
-
+import java.lang.Math;
 public class EnemyMissile extends MobileEntity {
 
 	public static final int base1x = 113, base1y = 611;
 	
 	EnemyMissile(int startX, int startY, int destination) {
-		super(startX, startY, 1, 1);
+		super(startX, startY, (int)(5*(Math.cos(Math.atan((base1y-startY)/(base1x-startX))))), (int)(5*(Math.sin(Math.atan((base1y-startY)/(base1x-startX))))));
 		setSprite("Z:\\git\\GayCommand\\MissileCommand\\Resources\\rock.png");
 		
 	}
